74ea6a33251ec263255ace18a5964d092afacc0c
Jiri Wolf <jiri.wolf@gmail.com>
2015-02-26 12:45:26 +0100

    Uprava modulu

---
+++ README_theme.php
@@ -0,0 +1,17 @@
+<?php
+
+// Theme - img
+// Theme - table
+
+// Odkaz
+l();
+
+// Text
+t();
+
+// Nastaveni cesty
+base_theme();
+path_to_theme();
+
+
+/* End of file theme.php */
---
+++ demo.sql
@@ -0,0 +1,6 @@
+create table demo_email (
+  id_email int not null auto_increment primary key,
+  jmeno varchar(40),
+  email varchar(40),
+  zprava text,
+)
